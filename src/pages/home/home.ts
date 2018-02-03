d20482c7bcb8bc5a46f7357201239ea9190ffa2a
Zac Littleberry <zaclittleberry@gmail.com>
2018-02-03 17:12:20 -0500

    add days and months. switch to 12 hour format

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,11 +13,36 @@ export class HomePage {
   date: string;
   time: string;
   interval: any;
+  months: string[];
+  days : string[];
 
   constructor(
     public navCtrl: NavController
   ) {
+    this.months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
+    this.days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
   }
 
   ngOnInit() {
@@ -28,17 +53,21 @@ export class HomePage {
   updateDateTime() {
     let today = new Date();
     let dd = today.getDate();
-    let mm = today.getMonth()+1; //January is 0!
+    let d = today.getDay();
+    let day = this.days[d];
+    let mm = today.getMonth(); //January is 0!
+    let month = this.months[mm];
 
     let yyyy = today.getFullYear();
 
     dd = this.zeroPad(dd);
     mm = this.zeroPad(mm);
 
-    this.date = mm + '/' + dd + '/' + yyyy;
+    this.date = day + ', ' + month + ' ' + dd + ', ' + yyyy;
 
     let h = today.getHours();
     let ampm = (h < 12) ? 'am' : 'pm';
+    h = h % 12; // 12 hour format
     h = (h || 12); // set h to 12 if 0
     let m = today.getMinutes();
     // add a zero in front of numbers<10
