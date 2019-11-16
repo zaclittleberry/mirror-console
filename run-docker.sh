9de003301fa586e12220aaeb70420761e5664f2c
zaclittleberry <zlittleberry@linuxfoundation.org>
2019-11-15 19:05:54 -0500

    try moving to bash source directory before running docker-compose up

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#!/bin/bash
+# where am i? move to where I am. This ensures source is properly sourced
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
 docker-compose up -d
 # docker-compose logs --follow
